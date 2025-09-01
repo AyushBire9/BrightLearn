64eac335418b06eeecd2b208fdcb289b0cdce0c5
AyushBire9 <ayushbire123@gmail.com>
2025-09-01 17:19:38 +0530

    change

--- brightlearn_backend/brightlearn_backend/settings.py
+++ brightlearn_backend/brightlearn_backend/settings.py
@@ -127,9 +127,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
-ALLOWED_HOSTS = ["ayushbire.pythonanywhere.com"]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'accounts.CustomUser'
